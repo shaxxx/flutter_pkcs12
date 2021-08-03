7824cd13c09d1ad6c81f4680ed574030b401dc88
shaxxx <isakota@gmail.com>
2021-08-03 14:38:22 +0200

    MacOs support

--- macos/Classes/FlutterPkcs12Plugin.swift
+++ macos/Classes/FlutterPkcs12Plugin.swift
@@ -1,5 +1,14 @@
 import Cocoa
 import FlutterMacOS
+import Foundation
+import Security
+import CommonCrypto
+
+enum SignatureHashType : integer_t {
+    case PKCS_SHA1 = 0
+    case PKCS_SHA256 = 1
+    case PKCS_SHA512 = 2
+}
 
 public class FlutterPkcs12Plugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,11 +18,159 @@ public class FlutterPkcs12Plugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+      do {
+            try executeFunc(call: call, result: result)
+        }  catch {
+            result(FlutterError(code: "UNKNOWN_ERROR", message: "Unknown exception occured.", details: error.localizedDescription ))
+        }
   }
+
+  private func executeFunc(call: FlutterMethodCall, result:FlutterResult) throws {
+        let dic = call.arguments as! [String: Any]
+        switch call.method {
+        case "signDataWithP12":
+            let p12Bytes = dic["p12Bytes"] as! FlutterStandardTypedData
+            let data = dic["data"] as! FlutterStandardTypedData
+            let signatureHashType = SignatureHashType.init(rawValue: dic["signatureHashType"] as! integer_t)!
+            try self.signDataWithP12(p12Bytes.data as NSData, dic["password"] as! NSString, data.data as NSData, signatureHashType, result)
+        case "readPublicKey":
+            let p12Bytes = dic["p12Bytes"] as! FlutterStandardTypedData
+            try self.readPublicKey(p12Bytes.data as NSData, dic["password"] as! NSString, result)
+        default:
+            print("Method not found")
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func signDataWithP12(_ p12Bytes: NSData, _ password: NSString, _ data: NSData, _ signatureHashType : SignatureHashType, _ result: FlutterResult) throws {
+        let privateKeyResponse = self.getPrivateKey(p12Bytes, password)
+        guard privateKeyResponse.error == errSecSuccess else {
+            let secError = privateKeyResponse.error
+            if secError == errSecAuthFailed {
+                result(FlutterError(code: "BAD_PASSWORD", message: "Invalid password", details: nil))
+            } else {
+                result(FlutterError(code: "FATAL_ERROR", message: "It was not possible to extract the certificate \(secError)", details: nil))
+            }
+            return
+        }
+        
+        guard let privateKey = privateKeyResponse.privateKey else {
+            result(FlutterError(code: "PRIVATE_KEY_ERROR", message: "It was not possible to extract the private key", details: nil))
+            return
+        }
+        let resultSign : RSASigningResult = self.sign(data: data as Data, privateKey: privateKey, signatureHashType: signatureHashType)
+        guard resultSign.error == nil else {
+            result(FlutterError(code: "ERROR_SIGN", message: "It was not possible to sign using the private key \(resultSign.error!.localizedDescription)", details: nil))
+            return
+        }
+        result(resultSign.signedData)
+    }
+
+    private func readPublicKey(_ p12Bytes: NSData, _ password: NSString, _ result: FlutterResult) throws {
+        let certificateResponse = self.getCertificate(p12Bytes, password)
+        guard certificateResponse.error == errSecSuccess else {
+            let secError = certificateResponse.error
+            if secError == errSecAuthFailed {
+                result(FlutterError(code: "BAD_PASSWORD", message: "Invalid password", details: nil))
+            } else {
+                result(FlutterError(code: "FATAL_ERROR", message: "It was not possible to extract the certificate \(secError)", details: nil))
+            }
+            return
+        }
+        guard let certificate = certificateResponse.certificate else {
+            result(FlutterError(code: "CERTIFICATE_ERROR", message: "It was not possible to extract the certificate", details: nil))
+            return
+        }
+        let data = SecCertificateCopyData(certificate) as Data
+        //let string = (data as Data).base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
+        result(data)
+    }
+
+    private typealias IdentityResult = (identity: SecIdentity?, error: OSStatus)
+    private func getIdentity(_ p12Bytes: NSData, _ password: NSString) -> IdentityResult{
+        let importPasswordOption:NSDictionary = [ kSecImportExportPassphrase : password]
+        var items : CFArray?
+        let secError : OSStatus = SecPKCS12Import(p12Bytes,importPasswordOption, &items)
+        guard secError == errSecSuccess else {
+            return IdentityResult( identity: nil, error: secError )
+        }
+        let identityDictionaries = items as! [[String: Any]]
+        let identity = identityDictionaries[0][kSecImportItemIdentity as String]
+            as! SecIdentity
+        return IdentityResult(identity: identity, error: errSecSuccess )
+    }
+    
+    private typealias PrivateKeyResult = (privateKey: SecKey?, error: OSStatus)
+    private func getPrivateKey(_ p12Bytes:NSData, _ password: NSString ) -> PrivateKeyResult {
+        let identityResult = getIdentity(p12Bytes, password)
+        guard identityResult.error == errSecSuccess else {
+            return PrivateKeyResult(privateKey: nil, error: identityResult.error)
+        }
+        
+        guard let identity = identityResult.identity else {
+            return PrivateKeyResult( privateKey: nil, error: errSecBadReq )
+        }
+        
+        var optPrivateKey: SecKey?
+        let secError = SecIdentityCopyPrivateKey(identity, &optPrivateKey)
+        guard secError == errSecSuccess else {
+            return PrivateKeyResult(privateKey: nil, error: secError )
+        }
+        guard let privateKey = optPrivateKey else {
+            return PrivateKeyResult(privateKey: nil, error: errSecBadReq )
+        }
+        return PrivateKeyResult(privateKey: privateKey, error: errSecSuccess )
+    }
+    
+    private typealias CertificateResult = (certificate: SecCertificate?, error: OSStatus)
+    private func getCertificate(_ p12Bytes: NSData, _ password: NSString) -> CertificateResult {
+        let identityResult = getIdentity(p12Bytes, password)
+        guard identityResult.error == errSecSuccess else {
+            return CertificateResult(certificate: nil, error: identityResult.error)
+        }
+        
+        guard let identity = identityResult.identity else {
+            return CertificateResult( certificate: nil, error: errSecBadReq )
+        }
+        
+        var optCertificateRef : SecCertificate?
+        let secError = SecIdentityCopyCertificate(identity, &optCertificateRef)
+        guard secError == errSecSuccess else {
+            return CertificateResult( certificate : nil, error: secError )
+        }
+        
+        guard let certificate = optCertificateRef else {
+            return CertificateResult( certificate : nil,  error: errSecBadReq )
+        }
+        
+        return CertificateResult( certificate : certificate, error: errSecSuccess)
+    }
+
+    private typealias RSASigningResult = (signedData: Data?, error: Error?)
+    private func sign(data plainData: Data, privateKey: SecKey!, signatureHashType: SignatureHashType) -> RSASigningResult {
+        // Then sign it
+        let err: OSStatus = errSecSuccess;
+        let algorithm: SecKeyAlgorithm
+        switch signatureHashType {
+        case SignatureHashType.PKCS_SHA1:
+            algorithm = .rsaSignatureMessagePKCS1v15SHA1
+        case SignatureHashType.PKCS_SHA256:
+            algorithm = .rsaSignatureMessagePKCS1v15SHA256
+        case SignatureHashType.PKCS_SHA512:
+            algorithm = .rsaSignatureMessagePKCS1v15SHA512
+        }
+        var error: Unmanaged<CFError>?
+        guard let signaturex = SecKeyCreateSignature(privateKey,
+                                                    algorithm,
+                                                    plainData as CFData,
+                                                    &error) as Data? else {
+            return (signedData: nil, error: error!.takeRetainedValue() as Optional<Error>)
+        }
+
+        if err == errSecSuccess {
+            return (signedData: Data(_: signaturex), error: nil)
+        }
+        
+    
+    }
 }
--- macos/flutter_pkcs12.podspec
+++ macos/flutter_pkcs12.podspec
@@ -5,18 +5,18 @@
 Pod::Spec.new do |s|
   s.name             = 'flutter_pkcs12'
   s.version          = '0.0.1'
-  s.summary          = 'A new flutter plugin project.'
+  s.summary          = 'Plugin to digitally sign data with PKCS12 certificates.'
   s.description      = <<-DESC
-A new flutter plugin project.
+  Plugin to digitally sign data with PKCS12 certificates.
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'https://github.com/shaxxx/flutter_pkcs12'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Integrator Dubrovnik' => 'integrator@integrator.hr' }
   s.source           = { :path => '.' }
   s.source_files     = 'Classes/**/*'
   s.dependency 'FlutterMacOS'
 
-  s.platform = :osx, '10.11'
+  s.platform = :osx, '10.12'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
   s.swift_version = '5.0'
 end
